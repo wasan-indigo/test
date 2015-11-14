0adb34ea833c7e604fe34fdb9bbfd9f6defee845
wasan <wasan.sudphan@gmail.com>
2015-11-14 15:00:21 +0700

    Commit Sound effect

--- app/src/main/java/com/indigocorp/san/butraffic/MainActivity.java
+++ app/src/main/java/com/indigocorp/san/butraffic/MainActivity.java
@@ -1,13 +1,51 @@
 package com.indigocorp.san.butraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // พื้นที่ ประกาศตัวแปล
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
+    // พื้นที่ ประกาศตัวแปล
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidget();
+
+        //Button Controler
+        buttonControler();
+
+    }  //main method
+
+    private void buttonControler() {
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Sound ef
+                MediaPlayer buttonPlayer = MediaPlayer.create(getBaseContext(), R.raw.dog);
+                buttonPlayer.start();
+
+                //Intent To Web
+
+
+            }
+        });
     }
-}
+
+    private void bindWidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+    }
+
+}    //main class
