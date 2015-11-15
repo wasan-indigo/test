2a525df63fd991ea10666bdbc8c04edc38178b45
wasan <wasan.sudphan@gmail.com>
2015-11-15 11:12:14 +0700

    List View

--- app/src/main/java/com/indigocorp/san/butraffic/MainActivity.java
+++ app/src/main/java/com/indigocorp/san/butraffic/MainActivity.java
@@ -28,7 +28,53 @@ public class MainActivity extends AppCompatActivity {
         //Button Controler
         buttonControler();
 
+        //Create ListView
+        createListView();
+
     }  //main method
+
+    private void createListView() {
+
+        //Setup Array
+        String[] strTitle = new String[20];
+        strTitle[0]="ห้ามเลี้ยวซ้าย";
+        strTitle[1]="ห้ามเลี่ยวขวา";
+        strTitle[2]="ตรงไป";
+        strTitle[3]="เลี้ยวขวา";
+        strTitle[4]="เลี้ยวซ้าย";
+        strTitle[5]="ทางออก";
+        strTitle[6]="ทางเข้า";
+        strTitle[7]="ทางออก";
+        strTitle[8]="หยุด";
+        strTitle[9]="จำกัดความสูง 2.5 ม.";
+        strTitle[10]="ทางแยก";
+        strTitle[11]="ห้ามกลับรถ";
+        strTitle[12]="ห้ามจอด";
+        strTitle[13]="รถวิ่งสวนทาง";
+        strTitle[14]="ห้ามแซง";
+        strTitle[15]="ทางเข้า";
+        strTitle[16]="หยุดตรวจ";
+        strTitle[17]="จำกัดความเร็ว";
+        strTitle[18]="จำกัดความกว้าง";
+        strTitle[19]="จำกัดความสูง 5.0 ม.";
+
+        int[] intIcon={R.drawable.traffic_01,R.drawable.traffic_02
+                      ,R.drawable.traffic_03,R.drawable.traffic_04
+                      ,R.drawable.traffic_05,R.drawable.traffic_06
+                      ,R.drawable.traffic_07,R.drawable.traffic_08
+                      ,R.drawable.traffic_09,R.drawable.traffic_11
+                      ,R.drawable.traffic_11,R.drawable.traffic_12
+                      ,R.drawable.traffic_13,R.drawable.traffic_14
+                      ,R.drawable.traffic_15,R.drawable.traffic_16
+                      ,R.drawable.traffic_17,R.drawable.traffic_18
+                      ,R.drawable.traffic_19,R.drawable.traffic_20};
+
+
+        MyAdapter objMyAdapter = new MyAdapter(MainActivity.this,strTitle,intIcon);
+        trafficListView.setAdapter(objMyAdapter);
+
+
+    } //Create ListView
 
     private void buttonControler() {
         aboutMeButton.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/indigocorp/san/butraffic/MyAdapter.java
+++ app/src/main/java/com/indigocorp/san/butraffic/MyAdapter.java
@@ -1,9 +1,12 @@
 package com.indigocorp.san.butraffic;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 /**
@@ -24,7 +27,7 @@ public class MyAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return 0;
+        return titelStrings.length;
     }
 
     @Override
@@ -39,6 +42,18 @@ public class MyAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+
+        LayoutInflater objLayoutInflater = (LayoutInflater) objContact.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View objView1 = objLayoutInflater.inflate(R.layout.my_listview,parent,false);
+        //Setup Title
+        TextView titleTextView = (TextView) objView1.findViewById(R.id.textView2);
+        titleTextView.setText(titelStrings[position]);
+        //titleTextView.setText(titelStrings[**int position**]);
+
+        //Setup Icon
+        ImageView iconImageView = (ImageView) objView1.findViewById(R.id.imageView);
+        iconImageView.setImageResource(iconInts[position]);
+
+        return objView1;
     }
 } // Main Class
